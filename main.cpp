e08782035d0d2a01c95f4e6b5d6ee629209a65f8
hualuo321 <15707220966@163.com>
2023-03-25 15:44:06 +0800

    main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "server/webserver.h"
 
 int main() {
-    /* 守护进程 后台运行 */
-    // daemon(1, 0); 
+    /* 开启守护进程 后台运行 */
+    daemon(1, 0); 
 
     WebServer server(
         1316, 3, 60000, false,                  // 端口，触发模式，超时时间，优雅关闭
